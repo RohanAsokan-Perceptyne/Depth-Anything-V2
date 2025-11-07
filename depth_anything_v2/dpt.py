73706a6a98e97e7459a3646e44d0e57a41e599e3
Rohan Asokan <rohan.a@perceptyne.com>
2025-11-07 23:49:11 +0530

    Modified function for working with EAM

--- depth_anything_v2/dpt.py
+++ depth_anything_v2/dpt.py
@@ -6,7 +6,7 @@ from torchvision.transforms import Compose
 
 from .dinov2 import DINOv2
 from .util.blocks import FeatureFusionBlock, _make_scratch
-from .util.transform import Resize, NormalizeImage, PrepareForNet
+from .util.transform import NormalizeImage, PrepareForNet, Resize
 
 
 def _make_fusion_block(features, use_bn, size=None):
@@ -45,9 +45,9 @@ class DPTHead(nn.Module):
         use_clstoken=False
     ):
         super(DPTHead, self).__init__()
-        
+
         self.use_clstoken = use_clstoken
-        
+
         self.projects = nn.ModuleList([
             nn.Conv2d(
                 in_channels=in_channels,
@@ -57,7 +57,7 @@ class DPTHead(nn.Module):
                 padding=0,
             ) for out_channel in out_channels
         ])
-        
+
         self.resize_layers = nn.ModuleList([
             nn.ConvTranspose2d(
                 in_channels=out_channels[0],
@@ -79,7 +79,7 @@ class DPTHead(nn.Module):
                 stride=2,
                 padding=1)
         ])
-        
+
         if use_clstoken:
             self.readout_projects = nn.ModuleList()
             for _ in range(len(self.projects)):
@@ -87,24 +87,24 @@ class DPTHead(nn.Module):
                     nn.Sequential(
                         nn.Linear(2 * in_channels, in_channels),
                         nn.GELU()))
-        
+
         self.scratch = _make_scratch(
             out_channels,
             features,
             groups=1,
             expand=False,
         )
-        
+
         self.scratch.stem_transpose = None
-        
+
         self.scratch.refinenet1 = _make_fusion_block(features, use_bn)
         self.scratch.refinenet2 = _make_fusion_block(features, use_bn)
         self.scratch.refinenet3 = _make_fusion_block(features, use_bn)
         self.scratch.refinenet4 = _make_fusion_block(features, use_bn)
-        
+
         head_features_1 = features
         head_features_2 = 32
-        
+
         self.scratch.output_conv1 = nn.Conv2d(head_features_1, head_features_1 // 2, kernel_size=3, stride=1, padding=1)
         self.scratch.output_conv2 = nn.Sequential(
             nn.Conv2d(head_features_1 // 2, head_features_2, kernel_size=3, stride=1, padding=1),
@@ -113,8 +113,8 @@ class DPTHead(nn.Module):
             nn.ReLU(True),
             nn.Identity(),
         )
-    
-    def forward(self, out_features, patch_h, patch_w):
+
+    def forward(self, out_features, patch_h=None, patch_w=None):
         out = []
         for i, x in enumerate(out_features):
             if self.use_clstoken:
@@ -123,30 +123,32 @@ class DPTHead(nn.Module):
                 x = self.readout_projects[i](torch.cat((x, readout), -1))
             else:
                 x = x[0]
-            
-            x = x.permute(0, 2, 1).reshape((x.shape[0], x.shape[-1], patch_h, patch_w))
-            
+
+            B, C, patch_h, patch_w = x.shape
+
+            # x = x.permute(0, 2, 1).reshape((x.shape[0], x.shape[-1], patch_h, patch_w))
+
             x = self.projects[i](x)
             x = self.resize_layers[i](x)
-            
+
             out.append(x)
-        
+
         layer_1, layer_2, layer_3, layer_4 = out
-        
+
         layer_1_rn = self.scratch.layer1_rn(layer_1)
         layer_2_rn = self.scratch.layer2_rn(layer_2)
         layer_3_rn = self.scratch.layer3_rn(layer_3)
         layer_4_rn = self.scratch.layer4_rn(layer_4)
-        
+
         path_4 = self.scratch.refinenet4(layer_4_rn, size=layer_3_rn.shape[2:])        
         path_3 = self.scratch.refinenet3(path_4, layer_3_rn, size=layer_2_rn.shape[2:])
         path_2 = self.scratch.refinenet2(path_3, layer_2_rn, size=layer_1_rn.shape[2:])
         path_1 = self.scratch.refinenet1(path_2, layer_1_rn)
-        
+
         out = self.scratch.output_conv1(path_1)
-        out = F.interpolate(out, (int(patch_h * 14), int(patch_w * 14)), mode="bilinear", align_corners=True)
+        out = F.interpolate(out, (int(patch_h * 16), int(patch_w * 16)), mode="bilinear", align_corners=True)
         out = self.scratch.output_conv2(out)
-        
+
         return out
 
 
